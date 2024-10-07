63b736863f9d194058ef5ae75fdb543d0d90d020
Brody730 <cjcr170504@gmail.com>
2024-10-07 15:45:57 -0600

    CREO YA ES LA ULTIMA XD

--- sysper/login.php
+++ sysper/login.php
@@ -17,7 +17,7 @@
     <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
     <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
     <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
 </head>
 
 <body>
