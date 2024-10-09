6cb988524c18b49d59546860f317dfc065dbac77
Brody730 <cjcr170504@gmail.com>
2024-10-09 10:55:41 -0600

    EL alex

--- template/login.php
+++ template/login.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es-MX">
 
 <head>
     <meta charset="utf-8">
-    <title>CyberMed</title>
-    <link rel="icon" href="logo.png" type="image/x-icon">
+    <title>Inicio de Sesion</title>
+    <link rel="icon" href="../imagenes/svg/cfe_icon.svg" type="image/x-icon">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
@@ -24,7 +24,7 @@
     <div class="container-fluid position-relative d-flex p-0">
         <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">Loading...</span>
+                <span class="sr-only">Cargando...</span>
             </div>
         </div>
 
@@ -34,9 +34,9 @@
                     <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
                         <div class="d-flex align-items-center justify-content-between mb-3">
                             <a href="index.html" class="">
-                                <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>CyberMed</h3>
+                                <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>SYSPER</h3>
                             </a>
-                            <h3>Sign In</h3>
+                            <h3>Entrar</h3>
                         </div>
                         <form action="control.php" method="POST">
                             <div class="form-floating mb-3">
@@ -45,17 +45,16 @@
                                  </div>
                             <div class="form-floating mb-4">
                                 <input type="password" class="form-control" id="floatingPassword" name="clave" placeholder="Password" required>
-                                <label for="floatingPassword">Password</label>
+                                <label for="floatingPassword">Contraseña</label>
                             </div>
                             <div class="d-flex align-items-center justify-content-between mb-4">
                                 <div class="form-check">
                                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    <label class="form-check-label" for="exampleCheck1">Recordarme</label>
                                 </div>
                                 <a href=""></a>
                             </div>
-                            <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
-                            <p class="text-center mb-0">Don't have an Account? <a href="signup.php">Sign Up</a></p>
+                            <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Iniciar sesion</button>
                         </form>
                     </div>
                 </div>
