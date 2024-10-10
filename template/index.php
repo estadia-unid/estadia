e7b8395924ecbceea948510299bdabe7c5317402
Brody730 <cjcr170504@gmail.com>
2024-10-10 14:33:15 -0600

    NOMBRE

--- template/index.php
+++ template/index.php
@@ -56,7 +56,7 @@ require_once("seguridad.php");
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
-                        <h6 class="mb-0"><?php echo $_SESSION['nombre']; ?></h6> <!-- Nombre del usuario -->
+                        <h6 class="mb-0"><?php echo $_SESSION['nombre_completo']; ?></h6> <!-- Nombre del usuario -->
                         <span><?php echo $_SESSION['categoria']; ?></span> <!-- Categoría del usuario -->
                     </div>
                 </div>
