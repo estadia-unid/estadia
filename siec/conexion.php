0d9c0524691fc2ba668c272a3b6ca7a16e63fe89
123KL <rrrdario@outlook.com>
2024-10-25 15:30:38 -0600

    sepa

--- siec/conexion.php
+++ siec/conexion.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
    date_default_timezone_set('America/Mexico_City');
-     $conecta =  mysqli_connect('localhost', 'siec', 'ctpalm2113', 'estadiaunid');
+     $conecta =  mysqli_connect('localhost', 'siec', 'ctpalm2113', 'siec');
      if(!$conecta){
          die('no pudo conectarse:' . mysqli_connect_error());
       }
