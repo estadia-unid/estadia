57863ba1ddc49498e162fe190143eedee198d0e0
Your Name <you@example.com>
2024-11-01 13:35:50 -0600

    cambios

--- siec/tabla_computadoras.php
+++ siec/tabla_computadoras.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 //include "conexion.php";
 ?>
 <!doctype html>
@@ -326,13 +327,16 @@
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Bienvenido</h1>
+        <h1 class="h2">computadoras</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group me-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary"><a href="tabla_computadoras.php">Todas</a></button>
+        <!--  
+        <div class="btn-group me-2">
+            
+          <button type="button" class="btn btn-sm btn-outline-secondary"><a href="tabla_computadoras.php">Todas</a></button>
             <button type="button" class="btn btn-sm btn-outline-secondary"><a href="tabla_computadoras.php?tabla=oficiales">Oficiales</a></button>
             <button type="button" class="btn btn-sm btn-outline-secondary"><a href="tabla_computadoras.php?tabla=no_oficiales">No oficiales</a></button>
           </div>
+          -->
           <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center gap-1">
             <svg class="bi"><use xlink:href="#calendar3"/></svg>
             This week
@@ -341,7 +345,7 @@
       </div>
 
     <!-- ya ajusta bien la tabla por favor -->
-    <h2>computadoras</h2>
+    <h2></h2>
 
 <div class="row g-4">
 
