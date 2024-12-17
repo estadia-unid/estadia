981ba9d051bcb38f87be84cd7b21374744916c84
Your Name <you@example.com>
2024-12-17 01:12:47 -0600

    despedida

--- siec/utilidades/pruebasdecositas.php
+++
@@ -1,7 +0,0 @@
-<?php
-$contraseña = "123R";
-$contraseñaCifrada = password_hash($contraseña, PASSWORD_BCRYPT);
-
-echo 'contraseña sin cifrar:     '. $contraseña . '<br>';
-echo 'contraseña cifrada:    ' . $contraseñaCifrada;
-?>
