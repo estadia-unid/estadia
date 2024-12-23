778188e5675b10e5201f7436eb21339de41f658d
Brody730 <127863094+Brody730@users.noreply.github.com>
2024-12-23 11:48:38 -0600

    Update registro.php

--- sysper/pages/registro.php
+++ sysper/pages/registro.php
@@ -148,7 +148,7 @@ mysqli_close($conecta);
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-calendar-grid-58 text-dark text-sm opacity-10"></i>
             </div>
-            <span class="nav-link-text ms-1">REGISTRO</span>
+            <span class="nav-link-text ms-1">SOLICITUD</span>
           </a>
         </li>
         <li class="nav-item">
